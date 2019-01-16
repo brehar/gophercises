2e3b86e5737613200732d997d6f03dc9f1ee09d0
Brett Hartman <brett.hartman@mac.com>
2019-01-15 22:33:31 -0800

    Complete urlshortener

---
+++ urlshortener/handler.go
@@ -0,0 +1,59 @@
+package urlshortener
+
+import (
+	"gopkg.in/yaml.v2"
+	"net/http"
+)
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+
+			return
+		}
+
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYAML(yamlBytes)
+
+	if err != nil {
+		return nil, err
+	}
+
+	pathsToUrls := buildMap(pathUrls)
+
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+
+	return pathsToUrls
+}
+
+func parseYAML(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+
+	err := yaml.Unmarshal(data, &pathUrls)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pathUrls, nil
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
