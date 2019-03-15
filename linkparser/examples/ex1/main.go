36cc644df057b7e2035bb0ab7a4277eb9faf9f15
Brett Hartman <brett.hartman@mac.com>
2019-03-15 15:19:36 -0700

    Complete HTML link parser using DFS

---
+++ linkparser/examples/ex1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"../../../linkparser"
+	"fmt"
+	"strings"
+)
+
+var exampleHtml = `
+<html>
+    <body>
+        <h1>Hello!</h1>
+        <a href="/other-page">
+			A link to another page
+			<span>some span</span>
+		</a>
+		<a href="/page-two">A link to a second page</a>
+    </body>
+</html>
+`
+
+func main() {
+	r := strings.NewReader(exampleHtml)
+	links, err := linkparser.Parse(r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", links)
+}
---
+++ linkparser/examples/ex4/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"../../../linkparser"
+	"fmt"
+	"strings"
+)
+
+var exampleHtml = `
+<html>
+    <body>
+        <a href="/dog-cat">dog cat <!-- commented text SHOULD NOT be included! --></a>
+    </body>
+</html>
+`
+
+func main() {
+	r := strings.NewReader(exampleHtml)
+	links, err := linkparser.Parse(r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", links)
+}
