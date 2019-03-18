ac2c5f38b80a8fec60275a1934f6bb3565f2920a
Brett Hartman <brett.hartman@mac.com>
2019-03-17 20:56:52 -0700

    Implement initialization of BoltDB for task manager

--- taskmanager/cmd/root.go
+++ taskmanager/cmd/root.go
@@ -1,19 +1,10 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 var RootCmd = &cobra.Command{
 	Use:   "task",
 	Short: "task is a CLI task manager",
-}
-
-func Execute() {
-	if err := RootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
 }
